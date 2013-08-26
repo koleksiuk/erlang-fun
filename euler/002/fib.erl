bb0a1c7465538670a5c9b292630a69392e84fcbd
Konrad Oleksiuk <konrad@up-next.com>
2013-08-26 13:58:57 +0200

    Update problem 002 - use <= for checking last number

--- euler/002/fib.erl
+++ euler/002/fib.erl
@@ -5,18 +5,15 @@
 run(Max) ->
   sum(filter(fun(X) -> X rem 2 =:= 0 end, fib(Max))).
 
-fib(Max) ->
-  case Max =:= 1 of
-    false ->
-      fib(1, 1, Max, [1,1]);
-    true ->
-      [1,1]
-  end.
+fib(Max) when Max > 1 ->
+  fib(1, 1, Max, [1,1]);
 
+fib(1) -> [1,1];
+fib(0) -> [].
 
 fib(M, N, Max, L) ->
   if
-    M + N < Max ->
+    M + N =< Max ->
       fib(N, M + N, Max, [M+N|L]);
     true ->
       reverse(L)
